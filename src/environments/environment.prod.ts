720eb3189f5f9ff604165fea93355b9233259b15
Oleksandra Tkachuk <lesyalesya9876@gmail.com>
2023-08-06 14:59:00 +0200

    #PN-16 add necessary data for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+
+firebase: {
+    apiKey: 'AIzaSyAX_xLsVcUymLyhkt2ZEi78bZY7ufuUE3o',
+    authDomain: 'clips-446c1.firebaseapp.com',
+    projectId: 'clips-446c1',
+    storageBucket: 'clips-446c1.appspot.com',
+    messaginSenderId: '195784608474',
+    appId: '1:195784608474:web:ea6f9ce88cf45801b4b3e3'
+},
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the 'fileReplacement' array.
+// 'ng build' replaces 'environments.ts' with 'environments.prod.ts'
+// the list of file replacement can be found in angular.json
+
+export const environment = {
+    production: false,
+
+firebase: {
+    apiKey: 'AIzaSyAX_xLsVcUymLyhkt2ZEi78bZY7ufuUE3o',
+    authDomain: 'clips-446c1.firebaseapp.com',
+    projectId: 'clips-446c1',
+    storageBucket: 'clips-446c1.appspot.com',
+    messaginSenderId: '195784608474',
+    appId: '1:195784608474:web:ea6f9ce88cf45801b4b3e3'
+},
+FIREBASE_APPCHECK_DEBUG_TOKEN: '16DACFBB-5F88-4CE6-9074-750A2137883B'
+}
